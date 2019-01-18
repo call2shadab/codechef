a906d176a738edafc99c7184fe5f5600096403d5
call2shadab <call2shadab@gmail.com>
2019-01-18 10:00:14 +0530

    Changes

--- Programs20182/WEB/main.js
+++ Programs20182/WEB/main.js
@@ -1,18 +1,20 @@
-var myGirl = [ 
-	{
-		"name": "Tina",
-		"fav-color": "black",
-		"fav-food": "burger"
-	},
-	{
-		"name": "Subhadra",
-		"fav-color": "blue",
-		"fav-food": "pizza"
-	}
-	{
-		"name": "Sangeeta",
-		"fav-color": "red",
-		"fav-food": "biryani"
-	}
-]
+var girlCon = document.getElementById("girls");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function() {
+	var req = new XMLHttpRequest();
+	req.open('GET', 'https://call2shadab.github.io/shacker/girls.json')
+	req.onload = function() {
+		var dat = JSON.parse(req.responseText);
+		renderHTML(dat);
+	};
+	req.send();
+	})
 
+function renderHTML(data) {
+	var htmlString = "";
+	for (i=0; i<data.length; i++) {
+		htmlString += "<p>" + data[i].name + " loves the " + data[i].fav_color + " and "+ data[i].fav_food + "</p>";
+	}
+	girlCon.insertAdjacentHTML('beforeend',htmlString);
+
+}
